53e42717c913294ef4008e82d0bc2d7ba6f1382d
DanishMir-A <zerobiinary@gmail.com>
2026-01-11 23:59:44 +0530

    Correct syntax for variable assignment in twoSum
    Fixed syntax error in twoSum method.

--- Arrays/2sum.java
+++ Arrays/2sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i<nums.length; i++){
-            int need = target - nums[i];
+            int need = target - nums[i]
             if(map.containsKey(need)){
                 return new int[]{map.get(need), i};
             }
